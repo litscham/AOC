f815fcf923a50c8c1c78df2477a57266c7ba582d
litscham <108621172+litscham@users.noreply.github.com>
2022-12-13 15:43:22 +0100

    Update Find-dub.py

--- 2022/day6/Find-dub.py
+++ 2022/day6/Find-dub.py
@@ -1,4 +1,4 @@
 #How many characters need to be processed before the first start-of-packet marker is detected?
 #string.find()?
 with open('input','r') as f:
-    data = f.readlines()
+    data = f.readlines().split("\n")
