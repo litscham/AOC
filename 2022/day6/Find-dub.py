1e2ee453d55af07b9da46572d54c4865021be285
litscham <m.litschauer@posteo.de>
2022-12-13 20:01:08 +0100

    solution day6 part1

--- 2022/day6/Find-dub.py
+++ 2022/day6/Find-dub.py
@@ -1,4 +1,10 @@
-#How many characters need to be processed before the first start-of-packet marker is detected?
-#string.find()?
-with open('input','r') as f:
-    data = f.readlines().split("\n")
+#identify the first position where the four most recently received characters were all differentHow many characters need to be processed before the first start-of-packet marker is detected?
+def find_dub():	
+	with open('input','r') as f:
+		s = f.read()
+		for i in range(len(s)):		
+			q = s[i:i+4]
+			x = set(q)
+			if len(q) == len(x):
+				return s.index(q)
+print(find_dub())
