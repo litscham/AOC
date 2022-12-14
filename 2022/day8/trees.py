7e4cc24736a3c5f1d657754e7ffa83345eaf9eb6
litscham <108621172+litscham@users.noreply.github.com>
2022-12-14 14:05:15 +0100

    Create trees.py

---
+++ 2022/day8/trees.py
@@ -0,0 +1,11 @@
+#shape 99x99
+#Edge=99+99+97+97=392
+vis_trees = set() # index (i,j) of a visible tree
+with open('input','r') as f: 
+    s = f.read()
+    s = s.split("\n") 
+    for i in range(2):
+        h =[*s.pop(i)]
+def cut_trees():	 
+        print(h)
+cut_trees()
