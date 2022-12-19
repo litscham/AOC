fc0a8fed1d35c88dee11ef208ff7ac18913d4594
litscham <108621172+litscham@users.noreply.github.com>
2022-12-19 15:55:06 +0100

    Update rope.py

--- 2022/day9/rope.py
+++ 2022/day9/rope.py
@@ -1,4 +1,5 @@
 #How many positions does the tail of the rope visit at least once?
+#from collections import OrderedDict
 def rope():	
     with open('input_test','r') as f:	
         #parse input		
@@ -9,19 +10,19 @@ def rope():
     idx = {'Hx':0,'Tx':0, 'Hy':0, 'Ty':0}
     c = set()
     for i in range(len(d)):
-        nH = 1
-        nT = 1
+        nH = 0
+        nT = 0
         if d[i][0] == 'R':
             print("right " + str(d[i][1]))
-            while nH <= int(d[i][1]):
+            while nH < int(d[i][1]):
                 print(nH)
                 idx['Hx'] += 1
                 nH = nH + 1
                 c.add((idx['Tx'],idx['Ty']))
-                if idx['Hx'] != idx['Tx']-1:
-                    idx['Tx'] += 1
-                    nT += 1
-                print(idx)
+                idx['Tx'] += 1
+                nT += 1
+                print("Head is at x =%d" %idx['Hx'])
+                print("Tail is at x =%d" %idx['Tx'])
         elif d[i][0] == 'L':
             print("left "+ str(d[i][1]))
             while nH <= int(d[i][1]):
@@ -39,7 +40,7 @@ def rope():
                 nH = nH + 1
                 c.add((idx['Tx'],idx['Ty']))
                 idx['Ty'] += 1
-                #nT += 1
+                nT += 1
         elif d[i][0] == 'D':
             print("down "+ str(d[i][1]))
             while nH <= int(d[i][1]):
