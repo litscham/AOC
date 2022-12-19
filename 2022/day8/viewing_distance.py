4e6e0489e7172e9a05e62e20fd409fe11c8a7b67
litscham <108621172+litscham@users.noreply.github.com>
2022-12-19 11:28:21 +0100

    Update viewing_distance.py
    print your way to success. also lot of loops

--- 2022/day8/viewing_distance.py
+++ 2022/day8/viewing_distance.py
@@ -1,69 +1,103 @@
-#how many trees are visible from outside the grid?
-def forest():	
-	with open('input','r') as f:	
-		#parse input		
-		s = f.read()
-		s = s.split("\n")
-		m = []		
-		for i in range(len(s)):
-			m.append(list(s[i]))	
-		m = m[:-1]
-		idx = set()
-		#edge		
-		for i in range(len(m)):
-			for j in range(len(m)):			
-				if i == 0 or j == 0:
-					idx.add((i, j))	
-				elif i == len(m)-1 or j == len(m)-1:
-					idx.add((i, j))	
-		#interior
-		print("left to right")
-		for i in range(1,len(m)):
-			print("i=%d" %i)
-			curr = int(m[i][0])
-			print("current is %d " % curr)
-			for j in range(1,len(m)):
-				print("j=%d" % j)
-				if curr < int(m[i][j]):
-					curr = int(m[i][j])
-					idx.add((i, j))
-					print("added")
-					print("current is %d " % curr)
-		print("down")
-		for j in range(1,len(m)):
-			print("j=%d" %j)
-			curr = int(m[0][j])
-			print("current is %d " % curr)
-			for i in range(1,len(m)):
-				print("i=%d" % i)
-				if curr < int(m[i][j]):
-					curr = int(m[i][j])
-					idx.add((i, j))
-					print("added")
-					print("current is %d " % curr)
-		print("right to left")
-		for i in range(1,len(m)-1,1):
-			print("i=%d" %i)
-			curr = int(m[i][len(m)-1])
-			print("current is %d " % curr)
-			for j in range(len(m)-2,-1,-1):
-				print("j=%d" % j)
-				if curr < int(m[i][j]):
-					curr = int(m[i][j])
-					idx.add((i, j))
-					print("added")
-					print("current is %d " % curr)
-		print("up")
-		for j in range(1,len(m)):
-			print("j=%d" %j)
-			curr = int(m[len(m)-1][j])
-			print("current is %d " % curr)
-			for i in range(len(m)-1,0,-1):
-				print("i=%d" % i)
-				if curr < int(m[i][j]):
-					curr = int(m[i][j])
-					idx.add((i, j))
-					print("added")
-					print("current is %d " % curr)
-		return len(idx)
-print(forest())
+#highest scenic score
+def viewing_distance():	
+    with open('input','r') as f:	
+        #parse input		
+        s = f.read()
+        s = s.split("\n")
+        m = []		
+        for i in range(len(s)):
+            m.append(list(s[i]))	
+        m = m[:-1]
+        curr = 0
+        scores = {}
+        for i in range(len(m)):
+            print("i=%d" %i)
+            for j in range (len(m)):
+                n = 1
+                score_right = 0
+                score_left = 0
+                score_up = 0
+                score_down = 0
+                print("j=%d" % j)
+                curr = int(m[i][j])
+                print("current is %d " % curr)
+                #right
+                print("Right")
+                while n+j <= len(m)-1:
+                    print("n=%d " % n)
+                    print("j+n=%d " % (j+n))
+                    if curr > int(m[i][j+n]):
+                            score_right = score_right+1
+                            n = n + 1
+                            print("yes!")
+                    elif curr == int(m[i][j+n]):
+                        score_right = score_right+1
+                        print("yes equal!")
+                        break
+                    else:
+                        print("no")
+                        score_right = score_right+1
+                        break
+                #left
+                print("left")
+                n = 1
+                while j-n >= 0:
+                    print("n=%d " % n)
+                    print("j-n=%d " % (j-n))
+                    if curr > int(m[i][j-n]):
+                        score_left = score_left+1
+                        n = n + 1
+                        print("yes!")
+                    elif curr == int(m[i][j-n]):
+                        score_left = score_left+1
+                        print("yes equal!")
+                        break
+                    else:
+                        print("no")
+                        score_left = score_left+1
+                        break
+                n=1
+                #down
+                print("down")
+                while i+n <= len(m)-1:
+                    print("n=%d " % n)
+                    print("i+n=%d " % (i+n))
+                    if curr > int(m[i+n][j]):
+                        score_down = score_down+1
+                        n = n + 1
+                        print("yes!")
+                    elif curr == int(m[i+n][j]):
+                        score_down = score_down+1
+                        print("yes equal!")
+                        break
+                    else:
+                        print("no")
+                        score_down = score_down+1
+                        break
+                #up
+                n = 1
+                print("up")
+                while i-n >= 0:
+                    print("n=%d " % n)
+                    print("i-n=%d " % (i-n))
+                    if curr > int(m[i-n][j]):
+                        score_up = score_up+1
+                        n = n + 1
+                        print("yes!")
+                    elif curr == int(m[i-n][j]):
+                        score_up = score_up+1
+                        print("yes equal!")
+                        break
+                    else:
+                        print("no")
+                        score_up = score_up+1
+                        break
+                scores.update({(i,j) : (score_right, score_left, score_down, score_up)})
+        #print(max(scores, key=scores.get),max(scores.values()))
+        #print(type(scores[(0,0)]))
+        tmp = list(scores.values())
+        scs = []
+        for i in range(len(tmp)):
+            scs.append(tmp[i][0]*tmp[i][1]*tmp[i][2]*tmp[i][3])
+        return max(scs)
+print(viewing_distance())
