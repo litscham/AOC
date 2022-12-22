2892528fe20c0b5d4c731a43552d1f3ec52a9c31
litscham <108621172+litscham@users.noreply.github.com>
2022-12-22 03:45:17 +0100

    Update clocknsignal.py

--- 2022/day10/clocknsignal.py
+++ 2022/day10/clocknsignal.py
@@ -1,8 +1,11 @@
-#ind the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
-Clock = 0
-If addx:
-Clock += 2
-V add (clock, int)
+#find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles. What is the sum of these six signal strengths?
+cycle = 0
+signal = 0
+If instruction[:3] in instructions is "addx":
+    cycle += 1
+    cycle += 1
+    signal += instruction[4:]
 
 Else:
-Clock += 1
+    cycle += 1
+return
